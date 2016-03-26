e68a09e4aa1bdb6eeedcc3810cb656d759dd7803
Mikhail Gerasimov <mgerasim@inbox.ru>
2016-03-26 22:03:02 +0000

    Заполнение модели Кадастр

--- Cadastr/Models/Cadastrentity.cs
+++ Cadastr/Models/Cadastrentity.cs
@@ -11,6 +11,20 @@ namespace Cadastr.Models
         public virtual DateTime created_at { get; set; }
         public virtual DateTime updated_at { get; set; }
 
+        public virtual DateTime delivery_at { get; set; } // Дата выдачи
+        public virtual string base_document { get; set; } // Документ основание
+        public virtual string law_subject { get; set; } // Субъект права
+        public virtual int law_object { get; set; } // Объект права: 1 - земельный участок, 2 - недвижимость
+
+        public virtual string category_land { get; set; } //Категория земли
+        public virtual double total_area { get; set; } // Общая площадь
+        public virtual string address { get; set; }
+        public virtual string law_type { get; set; } // Вид права
+        public virtual int cadastr_number {get; set;} //Кадастровый номер
+        public virtual string law_restriction { get; set; } // Ограничения права
+
+
+
         public virtual void Save()
         {
             this.created_at = DateTime.Now;
